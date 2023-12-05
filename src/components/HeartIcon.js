af26e71df0eef390b556693a65aa79abc0afabf0
Fariz <farizmohmd@gmail.com>
2023-12-06 01:09:22 +0530

    'third'

--- src/components/HeartIcon.js
+++ src/components/HeartIcon.js
@@ -1,4 +1,3 @@
-
 import React, {useState, useEffect} from 'react';
 import {
   View,
@@ -8,23 +7,33 @@ import {
   Dimensions,
 } from 'react-native';
 import * as Animatable from 'react-native-animatable';
-import Icon from 'react-native-vector-icons/FontAwesome'; 
+import Icon from 'react-native-vector-icons/FontAwesome';
 const Height = Dimensions.get('window').height;
 import Colors from '../assets/utilities/Colors';
 
 export const HeartIcon = () => {
   const [favorites, setFavorites] = useState([]);
   const [starred, setStarred] = useState([]);
+  const [showStar, setShowStar] = useState(false);
 
   const addToFavorites = item => {
     const updatedFavorites = [...favorites, item];
     setFavorites(updatedFavorites);
+
+    if (!showStar) {
+      setShowStar(true);
+
+      setTimeout(() => {
+        setShowStar(false);
+      }, 5000);
+    }
   };
 
   const removeFromFavorites = item => {
     const updatedFavorites = favorites.filter(fav => fav !== item);
     setFavorites(updatedFavorites);
     removeFromStarred(item);
+    setShowStar(false);
   };
 
   const starItem = item => {
@@ -49,74 +58,90 @@ export const HeartIcon = () => {
   const isItemFavorite = isFavorite(itemName);
   const isItemStarred = isStarred(itemName);
 
-  // const onAnimationEnd =()=>{
-  //   setTimeout(()=>{
-  //     // 
-  //   },5000)
-  // }
-  return (
-    <View style={styles.rowContainer}>
-    {isItemFavorite && (
-      <Animatable.View 
-      animation={{
-        0: {scale: 0, left: 0},
-        0.5: {scale: 1.2, left: -1},
-        1: {scale: 1, left: 5},
-      }}
-      duration={800}
-      easing='ease-in-out'
-      // onAnimationEnd={onAnimationEnd}
-      >
- <TouchableOpacity
-        onPress={() =>
-          isItemStarred ? unstarItem(itemName) : starItem(itemName)
-        }
-        activeOpacity={0.7}
-      >
-        <View style={styles.containerStar}>
-          <Icon
-            name={isItemStarred ? 'star' : 'star-o'}
-            size={Height * 0.023}
-            color={isItemStarred ? Colors.LIGHT_GREEN : Colors.GREEN}
-          />
-          <Text style={{fontSize:12}}>Clients</Text>
-        </View>
-      </TouchableOpacity>
-      </Animatable.View>
-     
-    )}
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowStar(false);
+    }, 5000);
 
-    <TouchableOpacity
-      onPress={() =>
-        isItemFavorite
-          ? removeFromFavorites(itemName)
-          : addToFavorites(itemName)
-      }
-      activeOpacity={0.7}
-    >
-      <View style={styles.container}>
-        <Icon
-          name={isItemFavorite ? 'heart' : 'heart-o'}
-          size={Height * 0.023}
-          color={isItemFavorite ? Colors.LIGHT_GREEN : Colors.GREEN}
-        />
-        {isItemFavorite && isItemStarred && (
-          <View style={styles.starLabel}>
-            <Icon
-              name="star"
-              size={Height * 0.015}
-              color={Colors.LIGHT_GREEN}
-            />
-          </View>
-        )}
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.fd}>
+        <View style={styles.margin}>
+          {isItemFavorite && (
+            <Animatable.View
+              animation={{
+                0: {scale: 0, left: 0},
+                0.5: {scale: 1.2, left: -35},
+                1: {scale: 1, left: -50},
+              }}
+              // animation='wobble'
+              duration={200}
+              easing="ease-in-out">
+              {showStar && (
+                <TouchableOpacity
+                  onPress={() =>
+                    isItemStarred ? unstarItem(itemName) : starItem(itemName)
+                  }
+                  activeOpacity={0.7}>
+                  <View style={styles.containerStar}>
+                    <Icon
+                      name={isItemStarred ? 'star' : 'star-o'}
+                      size={Height * 0.023}
+                      color={isItemStarred ? Colors.LIGHT_GREEN : Colors.GREEN}
+                    />
+                    <Text style={{fontSize: 11, fontWeight: 'bold'}}>
+                      Clients
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              )}
+            </Animatable.View>
+          )}
+        </View>
+
+        <View>
+          <TouchableOpacity
+            onPress={() =>
+              isItemFavorite
+                ? removeFromFavorites(itemName)
+                : addToFavorites(itemName)
+            }
+            activeOpacity={0.7}>
+            <Animatable.View
+              style={styles.containerHeart}
+              animation={isItemFavorite ? 'tada' : undefined}>
+              <Icon
+                name={isItemFavorite ? 'heart' : 'heart-o'}
+                size={Height * 0.023}
+                color={isItemFavorite ? Colors.LIGHT_GREEN : Colors.GREEN}
+              />
+              {isItemFavorite && isItemStarred && (
+                <View style={styles.starLabel}>
+                  <Icon
+                    name="star"
+                    size={Height * 0.015}
+                    color={Colors.LIGHT_GREEN}
+                  />
+                </View>
+              )}
+            </Animatable.View>
+          </TouchableOpacity>
+        </View>
       </View>
-    </TouchableOpacity>
-  </View>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: 'center',
+    marginBottom: 10,
+    justifyContent: 'center',
+  },
+  containerHeart: {
     alignItems: 'center',
     justifyContent: 'center',
     height: Height * 0.04,
@@ -127,6 +152,7 @@ const styles = StyleSheet.create({
     marginBottom: Height * 0.05,
     flexDirection: 'row',
     marginRight: Height * 0.01,
+    position: 'relative',
   },
   containerStar: {
     alignItems: 'center',
@@ -139,14 +165,17 @@ const styles = StyleSheet.create({
     marginBottom: Height * 0.05,
     backgroundColor: Colors.LIGHTER_GREEN,
     marginRight: Height * 0.01,
-  },
-  rowContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    position: 'absolute',
   },
   starLabel: {
     position: 'absolute',
     bottom: 0,
     right: 0,
+  },
+  margin: {
+    marginTop: -7,
+  },
+  fd: {
+    flexDirection: 'row',
   },
 });
